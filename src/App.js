6bb61e7d8a1ee2f6cbf5738642e5996adf33eaec
PilarSalaberry <pilarsalaberry@gmail.com>
2021-03-29 16:50:17 -0300

    agrego menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>Sol Azul | Macetas Artesanales</h1>
-      <p>Clase 2</p>
+      <h1 className="h1">Sol Azul | Macetas Artesanales</h1>
+      <ul className="list">
+        <li>
+          <a href="#" className="item">
+            Inicio
+          </a>
+        </li>
+        <li>
+          <a href="#" className="item">
+            Cemento
+          </a>
+        </li>
+        <li>
+          <a href="#" className="item">
+            Piedras
+          </a>
+        </li>
+        <li>
+          <a href="#" className="item">
+            Contacto
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
