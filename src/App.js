e0fbec72e24a2ba9ec9c8aabd616d6728302aa61
PilarSalaberry <pilarsalaberry@gmail.com>
2021-05-28 17:08:29 -0300

    detalles css

--- src/App.js
+++ src/App.js
@@ -36,6 +36,9 @@ function App() {
               <OrderForm />
             </Route>
           </Switch>
+          <footer className="footer">
+            <p className="p-footer">Todos los derechos reservados</p>
+          </footer>
         </div>
       </CartProvider>
     </Router>
